2fcdc93780c590703b3fbe95c5dfb55ae403c6b1
Grigorii Kokotkin <padgamerrfp@gmail.com>
2025-06-23 15:34:19 +0400

    3. split interface

---
+++ Connector.Application/Ports/IRestConnector.cs
@@ -0,0 +1,10 @@
+﻿using Connector.Domain.Entities;
+
+namespace Connector.Application.Ports;
+
+public interface IRestConnector
+{
+    Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount);
+    Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0);
+
+}
